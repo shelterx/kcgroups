1437bb0198f1d70c6b523718042e8ef2e6f3a882
shelterx <rickard@refused.net>
2026-04-15 15:16:21 +0200

    Initialize m_currentPid

--- foregroundbooster.h
+++ foregroundbooster.h
@@ -5,38 +5,37 @@
 #ifndef FOREGROUNDBOOSTER_H
 #define FOREGROUNDBOOSTER_H
 #include "boostersettings.h"
+#include <KApplicationScope>
 #include <QHash>
 #include <QObject>
 #include <QTimer>
 #include <tasksmodel.h>
-#include <KApplicationScope>
 
 class KApplicationScope;
 
-class ForegroundBooster : public QObject
-{
+class ForegroundBooster : public QObject {
 public:
-    ForegroundBooster(QObject *parent = nullptr);
-    ~ForegroundBooster();
+  ForegroundBooster(QObject *parent = nullptr);
+  ~ForegroundBooster();
 
 public Q_SLOTS:
-    void onActiveWindowChanged();
-    void onWindowRemoved(const QModelIndex &parent, int first, int last);
-    void onSwitchTimeout();
+  void onActiveWindowChanged();
+  void onWindowRemoved(const QModelIndex &parent, int first, int last);
+  void onSwitchTimeout();
 
 private:
-    TaskManager::TasksModel *m_tasksModel;
+  TaskManager::TasksModel *m_tasksModel;
 
-    BoosterSettings *m_settings;
-    uint m_currentPid;
-    QString m_currentAppid;
-    KApplicationScope *m_currentApp;
-    QTimer m_debounceTimer;
-    bool m_currentAppOrphaned = false;
-    QHash<uint, KApplicationScope *> m_appsByPid;
+  BoosterSettings *m_settings;
+  uint m_currentPid = 0;
+  QString m_currentAppid;
+  KApplicationScope *m_currentApp;
+  QTimer m_debounceTimer;
+  bool m_currentAppOrphaned = false;
+  QHash<uint, KApplicationScope *> m_appsByPid;
 
-   CGroupDeviceMemoryLimitList m_boostedGPUMemoryLimit;
-   CGroupDeviceMemoryLimitList m_nonBoostedGPUMemoryLimit;
+  CGroupDeviceMemoryLimitList m_boostedGPUMemoryLimit;
+  CGroupDeviceMemoryLimitList m_nonBoostedGPUMemoryLimit;
 };
 
 #endif // FOREGROUNDBOOSTER_H
